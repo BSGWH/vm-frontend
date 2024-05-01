6b79ee48d8c9a496d56e03391e23cae3ba77c7ba
weihaogu <weihao.gu@logicauto.io>
2024-05-01 13:01:39 -0700

    Edit company logo in dark mode

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,18 +1,26 @@
-import { cn } from "@/lib/utils";
+"use client";
 // import { MobileSidebar } from "./mobile-sidebar";
 // import { UserNav } from "./user-nav";
 import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 export default function Header() {
+  const { theme } = useTheme();
+
   return (
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
         <div className="hidden lg:block">
           <Link href={"/"}>
             <img
-              src={"/logos/oneRowBlack.png"}
+              src={
+                theme === "dark"
+                  ? "/logos/oneRowWhite.png"
+                  : "/logos/oneRowBlack.png"
+              }
               alt="Logo"
               className="w-40 h-auto object-cover"
             />
