9c9537eae5aa91e9edab1899a3fb21d4a3f0c4e2
evie-song <evie.shuangsong@gmail.com>
2024-05-01 11:18:53 -0500

    copied over session codes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
+import { FAQ } from "@/components/core/FAQ";
+import { Features } from "@/components/core/Features";
+import { Footer } from "@/components/core/Footer";
+import { Hero } from "@/components/core/Hero";
+import { HowItWorks } from "@/components/core/HowItWorks";
+import { Pricing } from "@/components/core/Pricing";
+import { ScrollToTop } from "@/components/core/ScrollToTop";
+import { Services } from "@/components/core/Services";
 import Header from "@/components/layout/header";
 import { Button } from "@/components/ui/button";
+import "./index.css";
+
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
+      <HowItWorks />
+      <Features />
+      <Services />
+      <Pricing />
+      <FAQ />
+      <Footer />
+      {/* <ScrollToTop /> */}
     </main>
   );
 }
