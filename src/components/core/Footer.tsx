a5fbb0ede06d0847fecbd143bf1b75032abda568
evie-song <evie.shuangsong@gmail.com>
2024-05-02 15:55:03 -0500

    changed logo in header and footer to use svg to better support dark and light mode transitions.

--- src/components/core/Footer.tsx
+++ src/components/core/Footer.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { useTheme } from "next-themes";
 import { buttonVariants } from "@/components/ui/button";
-import { Linkedin } from "lucide-react";
-import YouTubeIcon from "@mui/icons-material/YouTube";
 import XIcon from "@mui/icons-material/X";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Logo } from "../logo/Logo";
 
 interface CommunityProps {
   name: string;
@@ -31,7 +29,6 @@ const communities: CommunityProps[] = [
 ];
 
 export const Footer = () => {
-  const { theme } = useTheme();
 
   return (
     <footer id="footer">
@@ -39,16 +36,8 @@ export const Footer = () => {
 
       <section className="container py-20 grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-x-12 gap-y-8">
         <div className="col-span-full xl:col-span-2">
-          <a href="/" className="font-bold text-xl flex">
-            <img
-              src={
-                theme === "dark"
-                  ? "/logos/oneRowWhite.png"
-                  : "/logos/oneRowBlack.png"
-              }
-              alt="Logo"
-              className="w-6/12 object-cover object-top"
-            />
+          <a href="/" className="font-bold text-xl flex w-6/12">
+						<Logo  />						
           </a>
         </div>
 
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,24 +2,18 @@
 import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { Button } from "@/components/ui/button";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
+import { Logo } from "../logo/Logo";
+
 
 export default function Header() {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
 
   return (
     <div className="fixed top-0 left-0 right-0 supports-backdrop-blur:bg-background/60 border-b bg-background/95 backdrop-blur z-20">
       <nav className="h-14 flex items-center justify-between px-4">
-        <Link href={"/"}>
-          <img
-            src={
-              theme === "dark"
-                ? "/logos/oneRowWhite.png"
-                : "/logos/oneRowBlack.png"
-            }
-            alt="Logo"
-            className="w-40 h-auto object-cover"
-          />
+        <Link href={"/"} className="w-40 text-xl flex">
+          <Logo />
         </Link>
 
         <div className="flex items-center gap-2">
