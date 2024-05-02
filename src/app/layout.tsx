c73da77f084b71df56247c7f96e2bd1bd83f940a
evie-song <evie.shuangsong@gmail.com>
2024-05-02 14:58:55 -0500

    Reapply "added pricing section and icons in the footer"
    This reverts commit b0b1ceb8eecc57f3bbb3bb56ff8d5712efec9ac6.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <body className={inter.className}>
+        <Script async src="https://js.stripe.com/v3/pricing-table.js" />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
