b0860a42ae4ff77651490d709e3f3c3610339bd8
evie-song <evie.shuangsong@gmail.com>
2024-05-03 13:58:27 -0500

    added initial profile page

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -3,17 +3,18 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { SwitchForm } from "@/components/forms/SettingForm";
+import { ProfileForm } from "@/components/forms/ProfileForm";
 
-const breadcrumbItems = [{ title: "Settings", link: "/dashboard/profile" }]
+const breadcrumbItems = [{ title: "Profile", link: "/dashboard/profile" }]
 
 export default function page() {
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
         <DashboardBreadcrumb items={breadcrumbItems}/>
-        <Heading title="Settings" description="Manage account settings"/>
+        <Heading title="Profile" description="Manage profile"/>
         <Separator />
-        <SwitchForm />
+				<ProfileForm />
       </div>
     </ScrollArea>
   )
