553db2bd22240da0768c835ce033ca31564324d8
yichunhuangLogicAuto <yichun.huang@logicauto.io>
2024-08-27 22:28:45 -0500

    Implement Customer Dashboard

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -27,7 +27,7 @@ export function Calendar({ selectedDate, onDateSelect, serviceDates }: CalendarP
         week: "flex flex-row",
         weekday: "h-6 w-10 flex items-center justify-center",
         day: "h-10 w-10 flex items-center justify-center hover:bg-primaryCustomer hover:bg-opacity-50 hover:rounded-full",
-        today: `border border-primaryCustomer rounded-full`,
+        today: `text-primaryCustomer`,
         outside: 'text-gray-300',
         selected: `bg-primaryCustomer text-white rounded-full`,
         root: `${getDefaultClassNames().root} p-0 max-w-fit mx-auto`,
